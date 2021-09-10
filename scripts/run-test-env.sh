d35bf22d24c16041a4b9518591c144f794fd7353
Denys Asauliak <d.asauliak@gmail.com>
2021-09-10 20:24:31 +0300

    add useful script for running test docker image

---
+++ scripts/run-test-env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+CWDIR=$(dirname $(dirname $(realpath -s $0)))
+IMAGE=my/sensority-dev-image
+
+command -v docker > /dev/null
+if [ $? != 0 ]
+then
+    echo -e "Error: Docker isn't installed"
+    exit 1
+fi
+
+if [ -z $(docker images -q ${IMAGE}) ]
+then
+    echo -e "Error: Required image is absent"
+    exit 1
+fi
+
+docker run -it \
+--rm \
+--privileged \
+--user $(id -u):$(id -g) \
+--volume=${CWDIR}:${CWDIR}:rw \
+--network=bridge \
+--workdir=${CWDIR} \
+${IMAGE} /bin/bash
