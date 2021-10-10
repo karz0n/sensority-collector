289cfd8a6c994f8d68095a4064b2c8be6f8036d5
Denys Asauliak <d.asauliak@gmail.com>
2021-10-10 10:38:02 +0300

    add gtest tests

--- cmake/modules/AddGoogleTest.cmake
+++ cmake/modules/AddGoogleTest.cmake
@@ -7,29 +7,43 @@ endif()
 find_package(GTest CONFIG REQUIRED)
 
 if(TARGET GTest::gtest)
-    get_property(GTEST_LOCATION TARGET GTest::gtest PROPERTY LOCATION)
+    get_property(
+        GTEST_LOCATION
+        TARGET GTest::gtest
+        PROPERTY LOCATION
+    )
     message(STATUS "Found GTest: ${GTEST_LOCATION}")
 else()
     message(FATAL_ERROR "GTest library NOT found")
 endif()
 
 if(TARGET GTest::gmock)
-    get_property(GMOCK_LOCATION TARGET GTest::gmock PROPERTY LOCATION)
+    get_property(
+        GMOCK_LOCATION
+        TARGET GTest::gmock
+        PROPERTY LOCATION
+    )
     message(STATUS "Found GMock: ${GMOCK_LOCATION}")
 else()
     message(FATAL_ERROR "GMock library NOT found")
 endif()
 
-function(TARGET_ADD_TESTS)
-    set(options)
-    set(oneValueArgs TARGET)
-    set(multiValueArgs)
-    cmake_parse_arguments(
-        P "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
-    )
-    target_link_libraries(
-        ${P_TARGET} PRIVATE GTest::gtest GTest::gtest_main GTest::gmock
-                            GTest::gmock_main
-    )
-    gtest_add_tests(TARGET ${P_TARGET})
+function(gtest_enable)
+    set(prefix ARG)
+    set(noValues DEFAULT_MAIN)
+    set(singleValues TARGET DIRECTORY)
+    set(multiValues LIBS)
+    cmake_parse_arguments(${prefix} "${noValues}" "${singleValues}" "${multiValues}" ${ARGN})
+
+    if(NOT DEFINED ARG_TARGET)
+        message(FATAL_ERROR "The name of the target is mandatory argument")
+    endif()
+
+    list(APPEND ARG_LIBS GTest::gtest GTest::gmock)
+    if(ARG_DEFAULT_MAIN)
+        list(APPEND ARG_LIBS GTest::gtest_main GTest::gmock_main)
+    endif()
+    target_link_libraries(${ARG_TARGET} PRIVATE ${ARG_LIBS})
+
+    gtest_add_tests(TARGET ${ARG_TARGET} WORKING_DIRECTORY ${ARG_DIRECTORY})
 endfunction()
