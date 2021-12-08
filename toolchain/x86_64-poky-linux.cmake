033f7e1de25ad28ebadf2024e237b61b733573a9
Denys Asauliak <d.asauliak@gmail.com>
2021-12-08 20:56:14 +0200

    update toolchain file

--- toolchain/x86_64-poky-linux.cmake
+++ toolchain/x86_64-poky-linux.cmake
@@ -16,51 +16,55 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+# Set default paths to search program inside native part of SDK
+set(CMAKE_SYSTEM_PROGRAM_PATH ${NATIVE_SYSROOT}/usr/bin
+                              ${NATIVE_SYSROOT}/usr/sbin)
+
 set(NATIVE_PREFIX "${NATIVE_SYSROOT}/usr/bin/x86_64-poky-linux/x86_64-poky-linux-")
 
 set(CMAKE_C_COMPILER
     ${NATIVE_PREFIX}gcc
-    CACHE PATH "C compiler"
+    CACHE PATH ""
 )
 set(CMAKE_CXX_COMPILER
     ${NATIVE_PREFIX}g++
-    CACHE PATH "C++ compiler"
+    CACHE PATH ""
 )
 set(CMAKE_LINKER
     ${NATIVE_PREFIX}ld
-    CACHE PATH "linker"
+    CACHE PATH ""
 )
 set(CMAKE_STRIP
     ${NATIVE_PREFIX}strip
-    CACHE PATH "strip"
+    CACHE PATH ""
 )
 set(CMAKE_AR
     ${NATIVE_PREFIX}gcc-ar
-    CACHE PATH "archive"
+    CACHE PATH ""
 )
 set(CMAKE_NM
     ${NATIVE_PREFIX}gcc-nm
-    CACHE PATH "nm"
+    CACHE PATH ""
 )
 set(CMAKE_OBJCOPY
     ${NATIVE_PREFIX}objcopy
-    CACHE PATH "objcopy"
+    CACHE PATH ""
 )
 set(CMAKE_OBJDUMP
     ${NATIVE_PREFIX}objdump
-    CACHE PATH "objdump"
+    CACHE PATH ""
 )
 set(CMAKE_RANLIB
     ${NATIVE_PREFIX}ranlib
-    CACHE PATH "ranlib"
+    CACHE PATH ""
 )
 
-set(CMAKE_C_FLAGS
-    "-m64 -march=core2 -mtune=core2 -msse3 -mfpmath=sse"
+set(CMAKE_C_FLAGS_INIT
+    "-m64 -march=core2 -mtune=core2 -msse3 -mfpmath=sse -fstack-protector-strong -Wformat -Wformat-security -Werror=format-security"
     CACHE STRING "" FORCE
 )
-set(CMAKE_CXX_FLAGS
-    "-m64 -march=core2 -mtune=core2 -msse3 -mfpmath=sse"
+set(CMAKE_CXX_FLAGS_INIT
+    "-m64 -march=core2 -mtune=core2 -msse3 -mfpmath=sse -fstack-protector-strong -Wformat -Wformat-security -Werror=format-security"
     CACHE STRING "" FORCE
 )
 
