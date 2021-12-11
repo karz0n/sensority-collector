04cba43bc434c12f8b2db1b9265e47f3e3b6114e
Denys Asauliak <d.asauliak@gmail.com>
2021-12-11 22:18:37 +0200

    rework creating development docker

--- scripts/run-dev-env.sh
+++ scripts/run-dev-env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CONTR_PORT=2222
-CONTR_NAME=sensority-remote-env
-IMAGE_NAME=my/sensority-remote-dev
+CWDIR=$(dirname $(dirname $(realpath -s $0)))
+
+source $CWDIR/scripts/environment
 
 command -v docker > /dev/null
 if [ $? != 0 ]
@@ -11,26 +11,18 @@ then
     exit 1
 fi
 
-start()
-{
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:$CONTR_PORT" > /dev/null 2>&1
-    docker run -d -p127.0.0.1:$CONTR_PORT:22 --cap-add sys_ptrace --name $CONTR_NAME $IMAGE_NAME
-}
-
-stop()
-{
-    docker stop $CONTR_NAME
-}
-
-
-if [ "$1" = "start" ]; then
-    start
-    exit 0
+if [ -z $(docker images -q $DEV_IMAGE) ]
+then
+    echo -e "Error: Required image is absent"
+    exit 1
 fi
 
-if [ "$1" = "stop" ]; then
-    stop
-    exit 0
-fi
-
-echo -e "Error: Argument is missing or invalid"
+docker run -it \
+--rm \
+--privileged \
+--hostname=$USERNAME@dev \
+--user=$USER_UID:$USER_GID \
+--volume=${CWDIR}:${CWDIR}:rw \
+--network=bridge \
+--workdir=${CWDIR} \
+${DEV_IMAGE}
